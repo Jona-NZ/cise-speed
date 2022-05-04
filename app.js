582863924995d53f9f095f22e3a73db4f6caeb8c
Charles Monaghan <charles@charlesmonaghan.com>
2022-05-04 15:50:44 +1200

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// app.js
+
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+var cors = require("cors");
+// routes
+const books = require("./routes/api/books");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// use Routes
+app.use("/api/books", books);
+
+const port = process.env.PORT || 8082;
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ config/db.js
@@ -0,0 +1,21 @@
+// db.js
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const config = require("config");
+const db = process.env.MONGODB_CONNECTION_STRING;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+    });
+
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
