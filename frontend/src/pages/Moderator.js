b3c59feab63e2c840e7128208a4ae43a3ec4956b
Jona Stevenson <jonatutorials@gmail.com>
2022-05-18 22:51:09 +1200

    Initialize page structures

---
+++ frontend/src/pages/Moderator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Moderator = () => {
+  return (
+    <section className='section'>
+      <div>Moderator</div>
+    </section>
+  );
+};
+
+export default Moderator;
