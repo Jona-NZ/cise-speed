6e46eeb25766d0e7190bd8ffdbff55d7ad3d1076
Jona Stevenson <jonatutorials@gmail.com>
2022-05-19 16:13:02 +1200

    Fetch Mod Articles

--- frontend/src/pages/Moderator.js
+++ frontend/src/pages/Moderator.js
@@ -1,36 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../components/ModeratorCard';
+import { API_ENDPOINT } from '../api/index';
+import axios from 'axios';
 
 const Moderator = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${API_ENDPOINT}/api/articles`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => {
+        setArticles(res.data);
+        console.log(res);
+      })
+      .catch((err) => {
+        alert('Unable to retrieve articles: ' + err);
+      });
+  }, []);
+
   return (
-    <section className="section">
-      <div className="container">
-        <div class="columns">
-          <div class="column">
-            <Card journalName="this is a journal" author="test Author" />
+    <section className='section'>
+      <div className='container'>
+        <div class='columns'>
+          <div class='column'>
+            <Card journalName='this is a journal' author='test Author' />
           </div>
-          <div class="column">
-            <Card journalName="this is a journal" />
+          <div class='column'>
+            <Card journalName='this is a journal' />
           </div>
-          <div class="column">
-            <Card journalName="this is a journal" />
+          <div class='column'>
+            <Card journalName='this is a journal' />
           </div>
-          <div class="column">
-            <Card journalName="this is a journal" />
+          <div class='column'>
+            <Card journalName='this is a journal' />
           </div>
         </div>
-        <div class="columns">
-          <div class="column">
-            <Card journalName="this is a journal" />
+        <div class='columns'>
+          <div class='column'>
+            <Card journalName='this is a journal' />
           </div>
-          <div class="column">
-            <Card journalName="this is a journal" />
+          <div class='column'>
+            <Card journalName='this is a journal' />
           </div>
-          <div class="column">
-            <Card journalName="this is a journal" />
+          <div class='column'>
+            <Card journalName='this is a journal' />
           </div>
-          <div class="column">
-            <Card journalName="this is a journal" />
+          <div class='column'>
+            <Card journalName='this is a journal' />
           </div>
         </div>
       </div>
