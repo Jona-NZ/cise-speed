8f679767259a1d0ba807336e2862c2f828c6bd44
Jona Stevenson <jonatutorials@gmail.com>
2022-05-19 14:14:49 +1200

    Update DB names

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const ToBeModerated = require('../../models/ToBeModerated');
+
+router.post('/', (req, res) => {
+  ToBeModerated.create(req.body)
+    .then((book) => res.json({ msg: 'Article added successfully' }))
+    .catch((err) =>
+      res.status(400).json({ error: 'Unable to add this article' + err })
+    );
+});
+
+module.exports = router;
