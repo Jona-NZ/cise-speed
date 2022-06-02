0736de412eec2b636d6d762490d93bae4467e9d5
Charles Monaghan <charles@charlesmonaghan.com>
2022-06-02 13:13:44 +1200

    thank you

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
 var cors = require('cors');
+const http = require('http');
+
 // routes
 const articles = require('./routes/api/articles');
 const analyst = require('./routes/api/analyst');
+const admin = require('./routes/api/admin');
 
 const app = express();
 
@@ -22,6 +25,7 @@ app.use(express.json({ extended: false }));
 // use Routes
 app.use('/api/articles', articles);
 app.use('/api/analyst', analyst);
+app.use('/api/admin', admin);
 
 const port = process.env.PORT || 3000;
 
@@ -31,6 +35,13 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const httpServer = new http.Server(app);
+const server = httpServer.listen(port, () =>
+  console.log(`Server running on port ${port}`)
+);
 
-module.exports = app;
+// app.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports = {
+  server,
+};
--- tests/AnalystAPI.test.js
+++ tests/AnalystAPI.test.js
@@ -1,6 +1,17 @@
-const app = require('../app');
+const { server: app } = require('../app');
 const supertest = require('supertest');
 const request = supertest(app);
+const mongoose = require('mongoose');
+const connectDB = require('../config/db');
+
+beforeAll(() => {
+  return connectDB();
+});
+
+afterAll(() => {
+  app.close();
+  return mongoose.connection.close();
+});
 
 it("Gets the '/' endpoint", async () => {
   const res = await request.get('/api/analyst').send();
